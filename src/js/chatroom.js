69c202479d4818071e0775264be852d12f824d49
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-05-29 19:36:05 +0200

    first commit

---
+++ src/js/chatroom.js
@@ -0,0 +1,78 @@
+var stompClient = null;
+const txtMensaje = document.getElementById('mensaje').value;
+const textArea = document.getElementById('chat-textos');
+const divPrivados = document.getElementById('mensajes-privados');
+const escribiendoText = document.getElementById('escribiendoText');
+const email = document.getElementById('txt-email-socio').value;
+const socio = {
+    'email': localStorage.email
+};
+
+$(document).ready(function() {
+    connect();
+  });
+document.getElementById("btnEnviarPublico").addEventListener("click", sendPublicMessage);
+document.getElementById("btnEnviarPrivado").addEventListener("click", sendPrivMessage);
+
+function connect(){
+    var socket = new SockJS('http://localhost:8080/chat-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, onConnected);
+}
+
+function onConnected() {
+    stompClient.subscribe('/chat/publico', function(mensaje){
+        mostrarMensajePublico(JSON.parse(mensaje.body));
+    });
+
+    stompClient.send("/app/historial", {}, localStorage.email);
+
+    stompClient.subscribe('/chat/historial/'+localStorage.email, function(listaMensajes){
+        mostrarHistorial(JSON.parse(listaMensajes.body));
+        console.log(JSON.parse(listaMensajes.body));
+    });
+
+    stompClient.subscribe('/chat/'+ localStorage.email +'/private', function(mensaje){
+        mostrarMensajePrivado(JSON.parse(mensaje.body));
+    });
+}
+
+function sendPublicMessage(){
+    var mensaje = {
+        'id': 'id-' + new Date().getTime(),
+        'texto': $("#mensaje").val(),
+        'socio': socio
+    }
+
+    stompClient.send("/app/mensaje-publico", {}, JSON.stringify(mensaje));
+
+}
+
+function sendPrivMessage(){
+    const socioReceptor = {
+        'email': document.querySelector('#listado-emails').value
+    };
+    var mensaje = {
+        'id': 'id-' + new Date().getTime(),
+        'texto': $("#privMensaje").val(),
+        'socio': socio,
+        'receptor': socioReceptor
+    }
+
+    stompClient.send("/app/mensaje-privado", {}, JSON.stringify(mensaje));
+
+}
+
+function mostrarMensajePublico(mensaje){
+    textArea.innerHTML += '<div class="alert alert-info mx-3" role="alert"><h6>'+ mensaje.socio.nombre + ' ' +  mensaje.socio.apellido +'</h6>'+ mensaje.texto +'<small class="float-right">'+ mensaje.fecha.replace('T', ' ').substring(0,19) +'</small></div>'
+}
+
+function mostrarMensajePrivado(mensaje){
+    divPrivados.innerHTML += '<div class="alert alert-success alert-dismissible fade show mt-3" role="alert"><strong>'+ mensaje.socio.email +'</strong>: '+ mensaje.texto +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+}
+
+function mostrarHistorial(listaMensajes){
+    listaMensajes.forEach(mensaje => {
+        textArea.innerHTML += '<div class="alert alert-info mx-3" role="alert"><h6>'+ mensaje.socio.nombre + ' ' +  mensaje.socio.apellido +'</h6>'+ mensaje.texto +'<small class="float-right">'+ mensaje.fecha.replace('T', ' ').substring(0,19) +'</small></div>'
+    });
+}
---
+++ src/js/registro.js
@@ -0,0 +1,72 @@
+document.getElementById("btnRegistrar").addEventListener("click", registrarSocio);
+
+async function registrarSocio() {
+  const datos = {};
+  datos.id = new Date().getTime();
+  datos.nombre = document.getElementById('nombre').value;
+  datos.apellido = document.getElementById('apellidos').value;
+  datos.email = document.getElementById('email').value;
+  datos.password = document.getElementById('pass').value;
+  const repetirPassword = document.getElementById('repPass').value;
+  let valido = true;
+  if (!datos.nombre) {
+    document.querySelector('.error-nombre').textContent = 'El nombre no puede estar vacio';
+    valido = false;
+  } else {
+    document.querySelector('.error-nombre').textContent = '';
+    valido = true;
+  }
+  if (!datos.apellido) {
+    document.querySelector('.error-apellidos').textContent = 'El apellido no puede estar vacio';
+    valido = false;
+  } else {
+    document.querySelector('.error-nombre').textContent = '';
+    valido = true;
+  }
+  if (!datos.email) {
+    document.querySelector('.error-email').textContent = 'El email no puede estar vacio';
+    valido = false;
+  } else {
+    document.querySelector('.error-nombre').textContent = "";
+    valido = true;
+  }
+  if (!datos.password) {
+    document.querySelector('.error-contraseña').textContent = "La contraseña no puede estar vacia";
+    valido = false;
+  } else {
+    document.querySelector('.error-nombre').textContent = "";
+    valido = true;
+  }
+
+  if (repetirPassword !== datos.password) {
+    Swal.fire(
+      'Error',
+      'Las contraseñas no coinciden',
+      'error',
+    );
+    valido = false;
+  } else {
+    valido = true;
+  }
+
+  if (valido) {
+    const request = await fetch('http://localhost:8080/api/registro', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
+    }).then(response => {
+      console.log(response);
+      return response.json();
+    })
+      .then(data => {
+        console.log(data);
+        window.location.href = "login.html";
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+}
